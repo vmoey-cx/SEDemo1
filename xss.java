d601b190857496dffb802235d66a342e1b8cd135
vmoey-cx <71851881+vmoey-cx@users.noreply.github.com>
2020-11-02 09:02:35 +0000

    Update xss.java

--- xss.java
+++ xss.java
@@ -47,7 +47,7 @@ public class BookDetail_jsp extends HttpJspBase {
 
 		String userField = request.getParameter("Field"); 
 
-		printSanitizedName(userField);
+		printSanitizedName(userField); 
 	}
 
 	public static void loadComments () {
